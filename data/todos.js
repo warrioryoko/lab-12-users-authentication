37e8d1d0698f4af28c0b04a523fe14f25843c6f3
warrioryoko <miangpellegri@gmail.com>
2020-08-18 20:31:53 -0700

    Changed data files

---
+++ data/todos.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    todo: 'Wag the Dog',
+    completed: false,
+  },
+  {
+    todo: 'Teach Parrot profanity',
+    completed: false,
+  },
+  {
+    todo: 'Fart in a crowded elevator',
+    completed: false,
+  },
+  {
+    todo: 'Get Swifty in here',
+    completed: false,
+  },
+  {
+    todo: 'Teach Raccoons to eat potato chips',
+    completed: false,
+  }
+];
